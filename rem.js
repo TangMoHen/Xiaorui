4e66caeeef0f5e62ead4b995b897b043c4381734
TangMoHen <1401246860@qq.com>
2017-12-18 16:50:18 +0800

    Add files via upload

---
+++ rem.js
@@ -0,0 +1,4 @@
+document.documentElement.style.fontSize=100*document.documentElement.clientWidth/718+'px';
+window.onresize=function(){
+	document.documentElement.style.fontSize=100*document.documentElement.clientWidth/718+'px'
+};	//rem
---
+++ staff-details.js
@@ -0,0 +1,7 @@
+var mySwiper = new Swiper('.swiper-container',{
+	slidesPerView : 2,
+	spaceBetween : 15,
+	slidesOffsetBefore : 15,
+	slidesOffsetAfter : 15,
+	freeMode :true,
+}); 
